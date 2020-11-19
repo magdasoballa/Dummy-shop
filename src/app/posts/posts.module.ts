e1258027f82f30ea5de0ea419e31dc8880131c45
magda <soballamagda@gmail.com>
2020-11-19 21:20:54 +0100

    modules

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PostsComponent} from "./posts.component";
-import {PostsListComponent} from "./posts-list/posts-list.component";
-import {SinglePostComponent} from "./single-post/single-post.component";
+import { PostsComponent } from "./posts.component";
+import { PostsListComponent } from "./posts-list/posts-list.component";
+import { SinglePostComponent } from "./single-post/single-post.component";
 
 
 
